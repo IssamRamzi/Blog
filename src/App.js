c358e56b12c95ede667592c81467bbaa03e92fb1
Ramzi <iss.benahmed@outlook.com>
2024-04-19 18:31:14 +0200

    1st commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import About from "./components/About";
+import Blogs from "./components/Blogs";
+import SideBar from "./components/SideBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <div className="gradient"></div>
+      <SideBar />
+      <Switch>
+        <Router index path="/home">
+          <About />
+        </Router>
+
+        <Router path="/blogs">
+          <Blogs/>
+        </Router>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+export const SideBar = () => {
+    return (
+        <div className='container-bar'>
+            <div className="img">
+            </div>
+            <Link to="/home"><h2>kefelle</h2></Link>
+            <p id='under-title'>2nd year computer science student</p>
+
+            <ul>
+                <li><a href="https://github.com/IssamRamzi">github</a></li>
+                <li><a href="https://steamcommunity.com/id/kefelle/">steam</a></li>
+                <li><Link to="/blogs">blog</Link></li>
+                <li><a href="#">linkedin</a></li>
+            </ul>
+
+        </div>
+    )
+}
+
+
+export default SideBar;
