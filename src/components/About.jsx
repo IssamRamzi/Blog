18f8254b999e2cd63355145ede892db8f8f60d51
Ramzi <iss.benahmed@outlook.com>
2024-04-22 19:47:14 +0200

    Small fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div className='about'> 
-            <div className="container">
-                <br />
+  return (
+    <div className="about">
+      <div className="container">
+        <br />
 
+        <h2>about</h2>
 
-                <h2>about</h2>
-                
-                <p>my name is issam, you can find me online as kefelle, i have a strong interest in coding, electronics and computers in general.</p>
-                <p>this website serves as more or less dumping ground of useless information. here, you may find how to contact me, crap i've written and things that you might find interesting.</p>
+        <p>
+          my name is issam, you can find me online as kefelle, i have a strong
+          interest in coding, electronics and computers in general.
+        </p>
+        <p>
+          this website serves as more or less dumping ground of useless
+          information. here, you may find how to contact me, crap i've written
+          and things that you might find interesting.
+        </p>
 
-                <p>currently, the domain is looking like shit, i'm broke so the domain <em>might</em> change from time to time</p>
+        <p>
+          currently, the domain is looking like shit, i'm broke so the domain{" "}
+          <em>might</em> change from time to time
+        </p>
 
-                <h2>contact me</h2>
+        <h2>contact me</h2>
 
-                <p>i go by the name of <span>kefelle</span> on discord, feel free to message me, i'm not that interesting but a chat clearly won't hurt me :)</p>
+        <p>
+          i go by the name of <span>kefelle</span> on discord, feel free to
+          message me, i'm not that interesting but a chat clearly won't hurt me
+          :)
+        </p>
 
-                <p>besides discord you can contact me thru email, though, response may vary</p>
-            </div>
+        <p>
+          besides discord you can contact me thru email, though, response may
+          vary
+        </p>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default About
+export default About;
